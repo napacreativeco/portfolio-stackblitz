bfe8fcb379199e2e512d37267f71c2b01d4dd817
napacreativeco <74587022+napacreativeco@users.noreply.github.com>
2022-09-15 09:45:40 -0700

    organization

--- Components/Homepage/MoreInfo.js
+++ Components/Homepage/MoreInfo.js
@@ -6,6 +6,29 @@ import Headline from '../Headline.js';
 const selfie =
   'https://github.com/napacreativeco/portfolio-stackblitz/blob/main/assets/selfie.jpg?raw=true';
 
+export default function MoreInfo() {
+  return (
+    <MoreInfoCSS>
+      <div class="row">
+        <div class="image-container">
+          <img src={selfie} alt="" />
+        </div>
+        <p>
+          My name is James. I create fast and functional websites that can be
+          customized to your business's needs. I've been making websites for
+          over 6 years and creating graphics for a little less than that.
+        </p>
+
+        <div style={{ display: 'inline-block', paddingTop: '10px' }}>
+          <Link url="mailto:hello@napacreativeco.com">
+            <Headline>Contact me</Headline>
+          </Link>
+        </div>
+      </div>
+    </MoreInfoCSS>
+  );
+}
+
 const MoreInfoCSS = styled.section`
   text-align: center;
   padding: 100px 0;
@@ -41,26 +64,3 @@ const MoreInfoCSS = styled.section`
     }
   }
 `;
-
-export default function MoreInfo() {
-  return (
-    <MoreInfoCSS>
-      <div class="row">
-        <div class="image-container">
-          <img src={selfie} alt="" />
-        </div>
-        <p>
-          My name is James. I create fast and functional websites that can be
-          customized to your business's needs. I've been making websites for
-          over 6 years and creating graphics for a little less than that.
-        </p>
-
-        <div style={{ display: 'inline-block', paddingTop: '10px' }}>
-          <Link url="mailto:hello@napacreativeco.com">
-            <Headline>Contact me</Headline>
-          </Link>
-        </div>
-      </div>
-    </MoreInfoCSS>
-  );
-}
