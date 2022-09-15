eb99d197506d4c453198869c55dc6f9927def246
napacreativeco <74587022+napacreativeco@users.noreply.github.com>
2022-09-15 11:57:36 -0700

    1 file modified

--- Components/Homepage/MoreInfo.js
+++ Components/Homepage/MoreInfo.js
@@ -11,23 +11,24 @@ export default function MoreInfo() {
   return (
     <MoreInfoCSS>
       <div class="row">
-        <div class="image-container">
-          <Fade left>
-            <img src={selfie} alt="" />
-          </Fade>
-        </div>
+        <Fade left>
+          <div>
+            <div class="image-container">
+              <img src={selfie} alt="" />
+            </div>
+          </div>
+          <p>
+            My name is James. I create fast and functional websites that can be
+            customized to your business's needs. I've been making websites for
+            over 6 years and creating graphics for a little less than that.
+          </p>
 
-        <p>
-          My name is James. I create fast and functional websites that can be
-          customized to your business's needs. I've been making websites for
-          over 6 years and creating graphics for a little less than that.
-        </p>
-
-        <div style={{ display: 'inline-block', paddingTop: '10px' }}>
-          <Link url="mailto:hello@napacreativeco.com">
-            <Headline>Contact me</Headline>
-          </Link>
-        </div>
+          <div style={{ display: 'inline-block', paddingTop: '10px' }}>
+            <Link url="mailto:hello@napacreativeco.com">
+              <Headline>Contact me</Headline>
+            </Link>
+          </div>
+        </Fade>
       </div>
     </MoreInfoCSS>
   );
